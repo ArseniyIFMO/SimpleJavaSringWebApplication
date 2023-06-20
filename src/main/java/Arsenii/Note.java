4fc5566eb29bed8a68aaf919b75b1656df4d3a50
Arseniy <vatrushka.96@mail.ru>
2023-06-20 11:58:54 +0200

    Everything works, post + repository

--- src/main/java/Arsenii/Note.java
+++ src/main/java/Arsenii/Note.java
@@ -1,9 +1,17 @@
 package Arsenii;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 @Entity
 public class Note {
     public String value;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
 
     public Note(String value) {
         this.value = value;
